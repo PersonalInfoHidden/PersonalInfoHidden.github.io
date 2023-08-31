1854334d9b6068c5749611c9c6ccdac6fa28abf0
PersonalInfoHidden <106556443+PersonalInfoHidden@users.noreply.github.com>
2023-08-31 15:33:00 +0200

    fixed script

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const cPath = (dir) => {
     return `/${dir}/answers.html`;
 };
-document.addEventListener("DOMContentLoaded", () => {
-    const navElements = [
-        ["Home", "/"],
-        ["History", cPath("history")][("Http", cPath("http"))],
-        ["Tags", cPath("tags")],
-        ["Validation", cPath("validation")],
-        ["Valid Page", "/validhtml/valid.html"],
-    ];
 
-    let navUl = document.getElementById("navUl");
-    navUl.innerHTML = "";
+const navElements = [
+    ["Home", "/"],
+    ["History", cPath("history")][("Http", cPath("http"))],
+    ["Tags", cPath("tags")],
+    ["Validation", cPath("validation")],
+    ["Valid Page", "/validhtml/valid.html"],
+];
 
-    navElements.forEach((value) => {
-        navUl.innerHTML += `<li><a href="${value[1]}">${value[0]}</a></li>`;
-    });
+let navUl = document.getElementById("navUl");
+navUl.innerHTML = "";
+
+navElements.forEach((value) => {
+    navUl.innerHTML += `<li><a href="${value[1]}">${value[0]}</a></li>`;
 });
